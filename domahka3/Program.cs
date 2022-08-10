8e28bb233eb75065006600a892694a91c8ee4a9b
кристина <kristytitova@mail.ru>
2022-08-10 15:48:35 +0300

    Задача 6

--- domahka3/Program.cs
+++ domahka3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  4 -> да
+// -3 -> нет
+//  7 -> нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2 == 0)
+{
+    Console.WriteLine(number + " чётное число");
+}
+else
+{
+    Console.WriteLine(number + " нечётное число");
+}

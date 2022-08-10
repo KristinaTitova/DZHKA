9970f891d8b0550eebc41968ca30d2b560d2814c
кристина <kristytitova@mail.ru>
2022-08-10 15:48:56 +0300

    Задача 8

--- domahka4/Program.cs
+++ domahka4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i<=numberA) 
+{
+    if (i%2 == 0)
+    {
+         Console.Write(" " + i + " ");
+    }
+   i++;
+}

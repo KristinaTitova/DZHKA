9ed91fb1bf4846590f9ff2b0b091efb5954ec036
кристина <kristytitova@mail.ru>
2022-08-10 15:48:10 +0300

    Задача 4

--- domahka2/Program.cs
+++ domahka2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//   2 3 7 -> 7
+//   44 5 78 -> 78
+//   22 3 9 -> 22
+
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if ((a >= b) && (a >= c))
+{
+    Console.WriteLine("Число " + a + " максимальное");
+}
+else 
+if ((b >= a) && (b >= c))
+{
+    Console.WriteLine("Число " + b + " максимальное");
+}
+else 
+if ((c >= a) && (c >= b))
+{
+    Console.WriteLine("Число " + c + " максимальное");
+}
